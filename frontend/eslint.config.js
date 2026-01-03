8557cf9d9396ffae233f8a9d67ddf9e91bf02722
Vera Fesianava <verafes@gmail.com>
2026-01-02 16:36:25 -0800

    Integrate Prettier and update configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,13 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'vite.config.js']),
+  importPlugin.flatConfigs.recommended,
   {
     files: ['**/*.{js,jsx}'],
+    /* prettier-ignore */
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -22,8 +28,31 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: { react: { version: '19.2.0' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      prettier,
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/display-name': 'off',
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/self-closing-comp': 'warn',
+      'comma-dangle': ['error', 'only-multiline'],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      eqeqeq: 'error',
+      'import/no-unresolved': 'error',
+      'import/extensions': ['error', 'always', { js: 'never', mjs: 'never' }],
+      'import/no-duplicates': 'warn',
     },
   },
   {
@@ -34,4 +63,4 @@ export default defineConfig([
       },
     },
   },
-])
+]);
