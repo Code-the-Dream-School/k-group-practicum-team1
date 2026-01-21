93f79af807c7e43f94d08b6777cc5d565d8c207a
Val Lyzhyn <1instavl@gmail.com>
2026-01-21 15:22:40 -0800

    prettier fix

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
-import { MemoryRouter } from 'react-router-dom';
+import {MemoryRouter} from 'react-router-dom';
 
 describe('App', () => {
-  it('should render main heading section ', () => {
-    render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    );
+    it('should render main heading section ', () => {
+        render(
+            <MemoryRouter>
+                <App/>
+            </MemoryRouter>
+        );
 
-    const mainHeading = screen.getByText(/Get Your Auto Loan in/i);
-    expect(mainHeading).toBeInTheDocument();
-  });
-  it('should render benefits section', () => {
-    render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    );
+        const mainHeading = screen.getByText(/Get Your Auto Loan in/i);
+        expect(mainHeading).toBeInTheDocument();
+    });
+    it('should render benefits section', () => {
+        render(
+            <MemoryRouter>
+                <App/>
+            </MemoryRouter>
+        );
 
-    const benefitsHeading = screen.getByText(/Why Choose TurboLoan/i);
-    expect(benefitsHeading).toBeInTheDocument();
-  });
+        const benefitsHeading = screen.getByText(/Why Choose TurboLoan/i);
+        expect(benefitsHeading).toBeInTheDocument();
+    });
 
-  it('should render loan calculator section', () => {
-    render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    );
+    it('should render loan calculator section', () => {
+        render(
+            <MemoryRouter>
+                <App/>
+            </MemoryRouter>
+        );
 
-    const calculatorHeading = screen.getByText(/Calculate your loan/i);
-    expect(calculatorHeading).toBeInTheDocument();
-  });
+        const calculatorHeading = screen.getByText(/Calculate your loan/i);
+        expect(calculatorHeading).toBeInTheDocument();
+    });
 });
