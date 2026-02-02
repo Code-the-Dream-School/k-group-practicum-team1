57520ad6e484d05777d59054a12153c56cd89c3a
Elena Bychenkova <elena.bychenkova@gmail.com>
2026-02-01 20:55:16 -0800

    Extract ApplicationSerializer and fix application factory sequence

--- backend/spec/factories/applications.rb
+++ backend/spec/factories/applications.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :application do
     association :user
 
-    sequence(:application_number) { |n| "#AL-#{Time.current.year}-#{(10000 + n * 1000 + rand(1000)).to_s.rjust(5, '0')}" }
+    sequence(:application_number) { |n| "#AL-#{Time.current.year}-#{(10_000 + n * 1000 + rand(1000)).to_s.rjust(5, '0')}" }
 
     status { 'draft' }
     application_progress { 'personal' }
