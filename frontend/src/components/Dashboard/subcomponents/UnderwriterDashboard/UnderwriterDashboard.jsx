e33112bfa48289ffb1f9d7974287ed0220a68938
Vera Fesianava <verafes@gmail.com>
2026-02-10 12:47:00 -0800

    Fix Navbar dropdown and navigation

--- frontend/src/components/Dashboard/subcomponents/UnderwriterDashboard/UnderwriterDashboard.jsx
+++ frontend/src/components/Dashboard/subcomponents/UnderwriterDashboard/UnderwriterDashboard.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+// frontend/src/components/Dashboard/subcomponents/UnderwriterDashboard/UnderwriterDashboard
 
 function UnderwriterDashboard() {
-  return <div>Underwriter Dashboard (Placeholder)</div>;
+  return (
+    <div className="min-h-[calc(100vh-8rem)] flex flex-col items-center justify-center bg-gray-50 p-4">
+      <h1 className="text-2xl font-bold text-indigo-700 mb-4 ">Underwriter Dashboard (Placeholder)</h1>
+      <div>
+        <p className="text-gray-600 mt-2 block text-center">Loading ....</p>
+      </div>
+    </div>
+  );
 }
 
 export default UnderwriterDashboard;
