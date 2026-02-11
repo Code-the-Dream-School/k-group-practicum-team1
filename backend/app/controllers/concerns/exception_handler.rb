afaa4b4ecd5124d179597356690bb8eed0ca5d77
Vera Fesianava <verafes@gmail.com>
2026-02-10 18:30:31 -0800

    Add global exception handling

--- backend/app/controllers/concerns/exception_handler.rb
+++ backend/app/controllers/concerns/exception_handler.rb
@@ -1,19 +1,24 @@
 # app/controllers/concerns/exception_handler.rb
 module ExceptionHandler
-    # provides the more graceful `included` method
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    included do
-      rescue_from ActiveRecord::RecordNotFound do |e|
-        render json: { error: e.message }, status: :not_found
-      end
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { error: e.message }, status: :not_found
+    end
 
-      rescue_from ActiveRecord::RecordInvalid do |e|
-        render json: { error: e.message }, status: :unprocessable_entity
-      end
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
 
-      rescue_from ActionController::ParameterMissing do |e|
-        render json: { error: e.message }, status: :unprocessable_entity
-      end
+    rescue_from ActionController::ParameterMissing do |e|
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
+
+    rescue_from StandardError do |e|
+      Rails.logger.error e.full_message
+
+      render json: { error: "Something went wrong. Please try again later." }, status: :internal_server_error
     end
   end
+end
